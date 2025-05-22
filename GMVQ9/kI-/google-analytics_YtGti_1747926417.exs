561fdaa8befa1aaa53e9bd37b93dd39bb91d6513
saMueL593678 <hitters_roadies0w@icloud.com>
2025-05-22 16:06:59 +0100

    Reduce the file's memory footprint by optimizing data structures

---
+++ GMVQ9/kI-/google-analytics_YtGti_1747926417.exs
@@ -0,0 +1,12 @@
+[
+  plugins: [Phoenix.LiveView.HTMLFormatter],
+  import_deps: [:ecto, :ecto_sql, :phoenix, :polymorphic_embed],
+  subdirectories: ["priv/*/migrations"],
+  inputs: [
+    "*.{heex,ex,exs}",
+    "{config,lib,test,extra}/**/*.{heex,ex,exs}",
+    "priv/*/seeds.exs",
+    "storybook/**/*.exs"
+  ],
+  locals_without_parens: [assert_matches: 1]
+]
